6b4b3e9c67e02b9c71573610c1f0192586815ce1
“Samarth <samarthbm18@gmail.com>
2021-09-10 13:04:14 +0530

    [Samarth] Refactor: Used regex to validate the entered year

--- Basic core programs/LeapYear.py
+++ Basic core programs/LeapYear.py
@@ -2,23 +2,26 @@
 * @Author: Samarth BM.
 * @Date: 2021-09-08 21:19  
 * @Last Modified by: Samarth BM
-* @Last Modified time: 2021-09-08 21:34
+* @Last Modified time: 2021-09-10 13:01
 * @Title: To check whether entered year is leap year or not
 '''
 
-year = int(input("Enter a year to check"))
+import re
+
+year = input("Enter a year to check: ")
 
 # Condition to check for valid year.
-if (year <= 999 or year > 9999):
-    print("Enter a valid year")
-    
-# Condition to check for leap year.    
-else:
-    if year % 4 == 0:
-        if year % 100 != 0 or year % 400 == 0:
+if re.match("^[0-9]{4}$", year):
+
+    if int(year) % 4 == 0:
+        if int(year) % 100 != 0 or int(year) % 400 == 0:
             print(" Its a leap year")
         else:
             print("Its not a leap year")
     else:
          print("Its not a leap year")
+
+else:
+    print("Enter a valid year")
+   
 
