09cbbe2b8412bc11085874d7d720d6a20ea6522c
“Samarth <samarthbm18@gmail.com>
2021-09-10 12:53:47 +0530

    [Samarth] Refactor: Used regex to validate the entered name

--- Basic core programs/StringReplace.py
+++ Basic core programs/StringReplace.py
@@ -2,9 +2,11 @@
 * @Author: Samarth BM.
 * @Date: 2021-09-08 19:36  
 * @Last Modified by: Samarth BM
-* @Last Modified time: 2021-09-08 19:46
+* @Last Modified time: 2021-09-10 12:52
 * @Title: User Input and Replace String Template “Hello <<UserName>>, How are you?”
 '''
+
+import re 
 
 str = "Hello <<UserName>>, How are you?"
 
@@ -12,7 +14,7 @@ str = "Hello <<UserName>>, How are you?"
 name = input("Please enter your name: ")
 
 # Checking if length of name is greater or equal to 3.
-if len(name) >=3:
+if re.match("^[A-Z]{1}[a-z]{2,}$", name):
     new_str = str.replace('<<UserName>>', name )
     print(new_str)
 else:
