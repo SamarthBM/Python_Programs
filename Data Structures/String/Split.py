b83b7bda902ef0183708d9b0860d8326b0b9d556
“Samarth <samarthbm18@gmail.com>
2021-09-20 14:15:07 +0530

    [Samarth] Add: Program to sort the comma separated value.

---
+++ Data Structures/String/Split.py
@@ -0,0 +1,27 @@
+'''
+* @Author: Samarth BM.
+* @Date: 2021-09-20 14:10  
+* @Last Modified by: Samarth BM
+* @Last Modified time: 2021-09-20 14:10
+* @Title: To split a string. 
+'''
+
+from LogHandler import logger
+
+def split_string():
+    """
+    Description:
+        This function is to split a string.
+    """
+    try:
+        
+        string = 'https://www.w3resource.com/python-exercises/string'
+        
+        print(string.rsplit('/', 1)[0])
+        print(string.rsplit('-', 1)[0])
+
+    except Exception:
+        logger.error("Invalid Input")
+
+if __name__ == "__main__":
+    split_string()
